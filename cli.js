e9240a36a4f490bb650cd06696502243158b65c9
RobertAKARobin <robertgfthomas@gmail.com>
2019-01-09 12:27:44 -0600

    Prints image of each diff

--- cli.js
+++ cli.js
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
 
 const git = require('simple-git/promise')()
+const Jimp = require('jimp')
 
 async function main(){
+	const font = await Jimp.loadFont(Jimp.FONT_SANS_12_BLACK)
+	const imageWidth = 800
+	const imageBackground = 'fff'
+	const lineIndentPx = 5
+	const lineHeightPx = 16
+
 	const commits = JSON.parse(JSON.stringify((await git.log()).all))
-	for(let index = 0, commit = undefined; commit = commits[index]; index += 1){
-		let previousCommit = commits[index + 1]
+	commits.forEach(async (commit, commitIndex)=>{
+		const previousCommit = commits[commitIndex + 1]
 		if(previousCommit){
-			console.log(await git.diff([`${previousCommit.hash}..${commit.hash}`]))
+			const diffByLine = (await git.diff([`${previousCommit.hash}..${commit.hash}`])).split('\n')
+			const image = await (new Jimp(imageWidth, (lineHeightPx * (diffByLine.length - 1)), imageBackground))
+			diffByLine.forEach(async (line, lineIndex)=>{
+				image.print(font, lineIndentPx, (lineHeightPx * lineIndex), line)
+			})
+			await image.writeAsync(`./${commit.hash}.png`)
 		}
-	}
+	})
 }
 
 main()
