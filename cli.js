686807f9395dc3b4cef0e7c74f5181a29014c5f2
RobertAKARobin <robertgfthomas@gmail.com>
2019-01-09 11:23:53 -0600

    loop through commits and print diff of each

--- cli.js
+++ cli.js
@@ -3,8 +3,13 @@
 const git = require('simple-git/promise')()
 
 async function main(){
-	const log = await git.log()
-	console.log(log)
+	const commits = JSON.parse(JSON.stringify((await git.log()).all))
+	for(let index = 0, commit = undefined; commit = commits[index]; index += 1){
+		let previousCommit = commits[index + 1]
+		if(previousCommit){
+			console.log(await git.diff([`${previousCommit.hash}..${commit.hash}`]))
+		}
+	}
 }
 
 main()
